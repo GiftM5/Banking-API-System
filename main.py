d471d70d33d3287473c1e9d431d46d0b579296b8
FreakyJay1 <jjthebestmanmode@gmail.com>
2024-11-14 18:42:47 +0200

    add data in database for guys to work on

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
-from database.data import engine, Base
+from fastapi import FastAPI, Depends
+from database.data import engine, Base,SessionLocal
 from database.models.table import Account,Transaction
-
-
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 # Create tables
+class fetch_email(BaseModel):
+    email: str
+    name:str
+    
 Base.metadata.create_all(bind=engine)
 
+session = SessionLocal
+
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
+# def database(db: SessionLocal = Depends(get_db)):
+#     email = Session.query(Account).all()
+#     print(email)
+    
+# database(Account.email)
