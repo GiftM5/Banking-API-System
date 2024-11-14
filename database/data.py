ef17791d1f85ed55b611142c782db52fa94de371
FreakyJay1 <jjthebestmanmode@gmail.com>
2024-11-14 09:57:52 +0200

    fix

--- database/data.py
+++ database/data.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-database_url = os.getenv("DATABASE_URL")
+database_url = "sqlite:///data.db"
 
 # Creating the SQLAlchemy engine
 engine = create_engine(
