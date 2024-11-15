65cc3f9ea0bd4594748c359e2899bf6e4d483f4d
Mkhabele Mikejunior Mark Mashaole <mimkhabjhb024@student.wethinkcode.co.za>
2024-11-15 04:06:47 +0200

    added post & get request for creating and retrieving info

--- main.py
+++ main.py
@@ -3,27 +3,45 @@ from database.data import engine, Base,SessionLocal
 from database.models.table import Account,Transaction
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-# Create tables
-class fetch_email(BaseModel):
+
+#pydantic models
+class create_account_base(BaseModel):
     email: str
     name:str
     
+#create tables  
 Base.metadata.create_all(bind=engine)
 
-session = SessionLocal
-
+#innitialize fastapi
 app = FastAPI()
 
+#dependancy to get database session
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
-
-# def database(db: SessionLocal = Depends(get_db)):
-#     email = Session.query(Account).all()
-#     print(email)
+   
+#endpoint for creating acc
+@app.post('/account/create/')
+async def create_account(account:create_account_base, db:Session=Depends(get_db)):
+    email_match=db.query(Account).filter(Account.email==account.email).first()
+    if email_match:
+        print(email_match)
+        return {'message':'this email already exist','email':email_match}
+    else:
+        new_acc=Account(email=account.email,name=account.name)
+        db.add(new_acc)
+        db.commit()
+        db.refresh
+        return{"message": 'account created','email':account.email}
     
-# database(Account.email)
+#endpoint for retriving acc
+@app.get('/account/<account_id>')
+async def fetch_acccount(account_id:int, db:Session=Depends(get_db)):
+    _account=db.query(Account).filter(Account.id==account_id).first()
+    if _account:
+        return {'account_id': _account.id, 'email': _account.email, 'name': _account.name, 'balance': _account.balance}
+    else:
+        return {'mssage':'Account not found'}
