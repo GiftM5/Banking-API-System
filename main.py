ae42bea8790d33879f989c9bec38da1abff6d9f2
Mkhabele Mikejunior Mark Mashaole <mimkhabjhb024@student.wethinkcode.co.za>
2024-11-16 15:16:20 +0200

    email validation,surname return

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Depends,Path
 from database.data import engine, Base,SessionLocal
 from database.models.table import Account,Transaction
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from sqlalchemy.orm import Session
-import uuid
+
 #pydantic models
 class create_account_base(BaseModel):
     email: str
     name:str
+    surname:str
     
 #create tables  
 Base.metadata.create_all(bind=engine)
@@ -23,25 +24,35 @@ def get_db():
     finally:
         db.close()
    
-#endpoint for creating acc
+#endpoint for creating account
 @app.post('/account/create/')
 async def create_account(account:create_account_base, db:Session=Depends(get_db)):
     email_match=db.query(Account).filter(Account.email==account.email).first()
     if email_match:
-        print(email_match)
-        return {'message':'Account with this email already exist','email':email_match}
+        return {'message':'Account with this email already exist'}
     else:
-        new_acc=Account(email=account.email,name=account.name,account_number=int(uuid.uuid4().int % 1e10))
+        new_acc=Account(email=account.email,name=account.name,surname=account.surname)
         db.add(new_acc)
         db.commit()
         db.refresh
         return{"message": 'account created','email':account.email}
     
-#endpoint for retriving acc
+#endpoint for retriving spacific user data with id parameter
 @app.get('/account/{account_id}')
 async def fetch_acccount(account_id:int=Path(description="Account ID"), db:Session=Depends(get_db)):
     _account=db.query(Account).filter(Account.id==account_id).first()
     if _account:
-        return {'account Number': _account.account_number, 'email': _account.email, 'name': _account.name, 'balance': _account.balance}
+        return {'account Number': _account.account_number, 'email': _account.email, 'name': _account.name,'surname':_account.surname, 'balance': _account.balance,'account created':_account.creation_date}
     else:
         return {'mssage':'Account not found'}
+    
+#endpoint for updating information
+
+
+#endpoint for deleting an account 
+
+
+#endpoint for adding funds to account
+
+
+#endpoint for withdrawing fund from account
