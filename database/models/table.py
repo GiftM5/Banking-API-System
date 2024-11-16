16990d046ed5addc53090b2b5333bbd0cfd61c07
FreakyJay1 <jjthebestmanmode@gmail.com>
2024-11-16 11:41:29 +0200

    adding and fix the table

--- database/models/table.py
+++ database/models/table.py
@@ -1,14 +1,23 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey,Date
+from sqlalchemy import Column, Integer, String, Float, ForeignKey,Date,Enum
 from ..data import Base
 from sqlalchemy.orm import relationship
 from datetime import date
 import uuid
+
+
 class Account(Base):
     __tablename__ = 'accounts'
 
     id = Column(Integer, primary_key=True, index=True)
-    account_number=Column(Integer,default=int(uuid.uuid4().int % 1e10),unique=True,nullable=False)
+    account_number = Column(
+        Integer,
+        default=lambda: int(str(uuid.uuid4().int)[:10]),  
+        # The line 14 is generates a unique 10-digit number each time a new record is created.
+        unique=True,
+        nullable=False
+    )
     name = Column(String, index=True)
+    surname = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     balance = Column(Float, default=0.0)
     creation_date=Column(Date,default=date.today)
@@ -20,8 +29,8 @@ class Transaction(Base):
     __tablename__ = 'transactions'
 
     transaction_id = Column(Integer, primary_key=True, index=True)
-    account_id = Column(Integer, ForeignKey('accounts.id'))
+    account_id = Column(Integer, ForeignKey('accounts.account_number'))
     amount = Column(Float, nullable=False)
-    type = Column(String, nullable=False)  # 'deposit' or 'withdrawal'
+    type = Column(Enum('deposit', 'withdrawal', name='transaction_type'), nullable=False)
 
     account = relationship("Account", back_populates="transactions")
